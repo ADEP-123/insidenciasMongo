cb51c377678b18bccd09d89378e7bedc18bcd5c4
ADEP-123 <andep210399@gmail.com>
2023-08-26 15:56:22 -0500

    fix: :bug: Arreglando manejo de errores en los controladores delete

--- backend/src/controllers/deleteDataController.js
+++ backend/src/controllers/deleteDataController.js
@@ -5,12 +5,11 @@ const deleteAreasController = async (req, res, next) => {
     try {
         const { area_id } = req.query
         const result = await deleteAreaService(Number(area_id));
-        if(result.deletedCount == 0){
+        if (result.deletedCount == 0) {
             res.status(200).json({ message: `no se ha encontrado ningun area con ese id`, result })
-        }else {
+        } else {
             res.status(200).json({ message: `area eliminada con exito`, result })
         }
-        
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -19,9 +18,13 @@ const deleteAreasController = async (req, res, next) => {
 // Categorias
 const deleteCategoriasController = async (req, res, next) => {
     try {
-        const { area_id } = req.query
-        const result = await deleteCategoriaService(Number(area_id));
-        res.status(200).json({ message: `categoria eliminada con exito`, result })
+        const { cat_id } = req.query
+        const result = await deleteCategoriaService(Number(cat_id));
+        if (result.deletedCount == 0) {
+            res.status(500).json({ message: `no se ha encontrado ninguna categoria con ese id`, result })
+        } else {
+            res.status(200).json({ message: `categoria eliminada con exito`, result })
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -32,7 +35,11 @@ const deleteEquipoController = async (req, res, next) => {
     try {
         const { id_equipo } = req.query
         const result = await deleteEquipoService(Number(id_equipo));
-        res.status(200).json({ message: `Equipo eliminado con exito`, result })
+        if (result.deletedCount == 0) {
+            res.status(500).json({ message: `no se ha encontrado ninguna equipo con ese id`, result })
+        } else {
+            res.status(200).json({ message: `Equipo eliminado con exito`, result })
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -43,7 +50,11 @@ const deleteInsidenciasController = async (req, res, next) => {
     try {
         const { id_insi } = req.query
         const result = await deleteInsidenciaService(Number(id_insi));
-        res.status(200).json({ message: `Insidencia eliminada con exito`, result })
+        if (result.deletedCount == 0) {
+            res.status(500).json({ message: `no se ha encontrado ninguna insidencia con ese id`, result })
+        } else {
+            res.status(200).json({ message: `Insidencia eliminada con exito`, result })
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -54,7 +65,11 @@ const deleteLugaresController = async (req, res, next) => {
     try {
         const { lugar_id } = req.query
         const result = await deleteLugaresController(Number(lugar_id));
-        res.status(200).json({ message: `Lugar eliminado con exito`, result })
+        if (result.deletedCount == 0) {
+            res.status(500).json({ message: `no se ha encontrado ningun lugar con ese id`, result })
+        } else {
+            res.status(200).json({ message: `Lugar eliminado con exito`, result })
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -65,7 +80,11 @@ const deleteTipoEquipoController = async (req, res, next) => {
     try {
         const { tip_equip_id } = req.query
         const result = await deleteTipo_equipoService(Number(tip_equip_id));
-        res.status(200).json({ message: `Tipo de equipo eliminado con exito`, result })
+        if (result.deletedCount == 0) {
+            res.status(500).json({ message: `no se ha encontrado ningun tipo de equipo con ese id`, result })
+        } else {
+            res.status(200).json({ message: `Tipo de equipo eliminado con exito`, result })
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -76,7 +95,11 @@ const deleteTiposController = async (req, res, next) => {
     try {
         const { tip_id } = req.query
         const result = await deleteTipoService(Number(tip_id));
-        res.status(200).json({ message: `tipo de insidencia eliminado con exito`, result })
+        if (result.deletedCount == 0) {
+            res.status(500).json({ message: `no se ha encontrado ningun tipo de insidencia con ese id`, result })
+        } else {
+            res.status(200).json({ message: `tipo de insidencia eliminado con exito`, result })
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -87,7 +110,11 @@ const deleteTrainerController = async (req, res, next) => {
     try {
         const { train_id } = req.query
         const result = await deleteTrainerService(Number(train_id));
-        res.status(200).json({ message: `trainer eliminado con exito`, result })
+        if (result.deletedCount == 0) {
+            res.status(500).json({ message: `no se ha encontrado ningun trainer con ese id`, result })
+        } else {
+            res.status(200).json({ message: `trainer eliminado con exito`, result })
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
