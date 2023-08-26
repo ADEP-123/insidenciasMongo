2fb6bd973c14855ae0c7b451be7aaa3a7444fec0
ADEP-123 <andep210399@gmail.com>
2023-08-26 18:00:48 -0500

    feat: :sparkles: Content middleware lugares

--- backend/src/middleware/contentVerifyMiddleware.js
+++ backend/src/middleware/contentVerifyMiddleware.js
@@ -49,9 +49,20 @@ const contentMiddlewareInsidencias = (req, res, next) => {
     (!Verify) ? res.status(406).send({ status: 406, message: "No Autorizado" }) : next();
 };
 
+const contentMiddlewareLugares = (req, res, next) => {
+    let { payload } = req.data;
+    const { iat, exp, ...newPayload } = payload;
+    const { entidad, ...finalPayload } = newPayload;
+    payload = finalPayload;
+    let Clone = JSON.stringify(classToPlain(plainToClass(lugaresDTO, {}, { ignoreDecorators: true })));
+    let Verify = Clone === JSON.stringify(payload);
+    (!Verify) ? res.status(406).send({ status: 406, message: "No Autorizado" }) : next();
+};
+
 export {
     contentMiddlewareAreas,
     contentMiddlewareCategorias,
     contentMiddlewareEquipo,
-    contentMiddlewareInsidencias
+    contentMiddlewareInsidencias,
+    contentMiddlewareLugares
 }
