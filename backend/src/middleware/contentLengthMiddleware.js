0d46af15e3f7d31d443b33fc3576bb223539d78b
ADEP-123 <andep210399@gmail.com>
2023-08-26 19:39:35 -0500

    feat: :sparkles: middleware content lentgth insidencias

--- backend/src/middleware/contentLengthMiddleware.js
+++ backend/src/middleware/contentLengthMiddleware.js
@@ -25,12 +25,19 @@ const middlewareContentLengthEquipo = (req, res, next,) => {
         }) : next()
 };
 
-
+const middlewareContentLengthInsidencias = (req, res, next,) => {
+    console.log(req.headers['content-length']);
+    req.headers['content-length'] > 450 ?
+        res.status(413).send({
+            status: 413,
+            message: "El tamaño de la informacion enviada es incorrecta"
+        }) : next()
+};
 
 
 export {
     middlewareContentLengthAreas,
     middlewareContentLengthCategorias,
     middlewareContentLengthEquipo,
-    
+    middlewareContentLengthInsidencias
 }
--- backend/src/routes/postData.js
+++ backend/src/routes/postData.js
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import { postAreasController, postCategoriasController, postEquipoController, postInsidenciasController, postLugaresController, postTipoEquipoController, postTiposController, postTrainerController } from '../controllers/postDateController.js';
 import { middlewarePostPutAreasDTO, middlewarePostPutCategoriasDTO, middlewarePostPutEquipoDTO, middlewarePostPutInsidenciasDTO, middlewarePostPutLugaresDTO, middlewarePostPutTipoEquipoDTO, middlewarePostPutTiposDTO, middlewarePostPutTrainerDTO } from '../middleware/middlewareDTO.js';
 import { contentMiddlewareAreas, contentMiddlewareCategorias, contentMiddlewareEquipo, contentMiddlewareInsidencias, contentMiddlewareLugares, contentMiddlewareTipoEquipo, contentMiddlewareTipo, contentMiddlewareTrainer } from "../middleware/contentVerifyMiddleware.js";
-import { middlewareContentLengthAreas, middlewareContentLengthCategorias,middlewareContentLengthEquipo } from '../middleware/contentLengthMiddleware.js';
+import { middlewareContentLengthAreas, middlewareContentLengthCategorias,middlewareContentLengthEquipo, middlewareContentLengthInsidencias } from '../middleware/contentLengthMiddleware.js';
 
 const postInitRoute = () => {
     const router = Router();
     router.post("/areas", middlewareContentLengthAreas, contentMiddlewareAreas, middlewarePostPutAreasDTO, postAreasController);
     router.post("/categorias", middlewareContentLengthCategorias, contentMiddlewareCategorias, middlewarePostPutCategoriasDTO, postCategoriasController);
     router.post("/equipo", middlewareContentLengthEquipo, contentMiddlewareEquipo, middlewarePostPutEquipoDTO, postEquipoController);
-    router.post("/insidencias", contentMiddlewareInsidencias, middlewarePostPutInsidenciasDTO, postInsidenciasController);
+    router.post("/insidencias",middlewareContentLengthInsidencias, contentMiddlewareInsidencias, middlewarePostPutInsidenciasDTO, postInsidenciasController);
     router.post("/lugares", contentMiddlewareLugares, middlewarePostPutLugaresDTO, postLugaresController);
     router.post("/tipo_equipo", contentMiddlewareTipoEquipo, middlewarePostPutTipoEquipoDTO, postTipoEquipoController);
     router.post("/tipos", contentMiddlewareTipo, middlewarePostPutTiposDTO, postTiposController);
